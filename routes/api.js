33992431d14cfbf11f1def8461880f0b16bea9b6
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-10-21 21:57:32 -0400

    Remove "/api" from router URLs (already in express route in server.js);

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
+router.post("/transaction", ({body}, res) => {
+  console.log("transaction post body", body);
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -11,7 +12,7 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -21,7 +22,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
-router.get("/api/transaction", (req, res) => {
+router.get("/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
